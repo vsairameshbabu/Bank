7c998b2dee9534e058d6777ffe51c6cce398310b
vsairameshbabu <vsairameshbabu27@gmail.com>
2025-07-14 07:58:51 +0530

    cors config added

--- BankManagement/src/main/java/com/app/controller/AccountController.java
+++ BankManagement/src/main/java/com/app/controller/AccountController.java
@@ -6,19 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.app.entities.Account;
 import com.app.service.AccountService;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/account")
 public class AccountController {
@@ -55,14 +48,14 @@ public class AccountController {
 		Account account = service.depositAmount(accountNumber, amount);
 		return account;	
 	}
-	
+
 	@PutMapping("/withdraw/{accountNumber}/{amount}")
 	public Account withdrawAccount(@PathVariable Long accountNumber, @PathVariable Double amount) {
-		Account account = service.depositAmount(accountNumber, amount);
+		Account account = service.withdrawAmount(accountNumber, amount);
 		return account;
 	}
-	
-	
+
+
 	@DeleteMapping("/delete/{accountNumber}")
 	public ResponseEntity<String> deleteAccount(@PathVariable Long accountNumber) {
 		service .closeAccount(accountNumber);
